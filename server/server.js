fc5e0a3765d14b2b1c9ddb5f1bcdc9a225e9206c
TheBestDon <donatas_bakanas@yahoo.com>
2017-05-10 15:39:51 +0300

    server change

--- server/server.js
+++ server/server.js
@@ -1,55 +1,56 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-const {ObjectID} = require('mongodb');
-
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-    var todo = new Todo({
-        text: req.body.text
-    });
+  var todo = new Todo({
+    text: req.body.text
+  });
 
-todo.save().then((doc) => {
+  todo.save().then((doc) => {
     res.send(doc);
-}, (e) => {
+  }, (e) => {
     res.status(400).send(e);
-});
+  });
 });
 
 app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
-        res.send({todos});
-    }, (e) => {
-        res.status(400).send(e);
-    });
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
 app.get('/todos/:id', (req, res) => {
-    var id = req.params.id;
+  var id = req.params.id;
 
-    if(!ObjectID.isValid(id)) {
-        res.status(404).send();
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
     }
 
-    Todo.findById(id).then((todo) => {
-        if(!todo) {
-            return res.status(404).send();
-        }
-        res.send({todo});
-    }).catch((e) => {res.status(404).send()});
-    
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Started on port ${port}`);
+  console.log(`Started up at port ${port}`);
 });
 
 module.exports = {app};
